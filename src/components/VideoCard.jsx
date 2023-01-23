f76612abcfb94f45981aaf31be40b1cd217cdd9b
Eeejeong Kim <ennea9on@gmail.com>
2023-01-23 14:53:49 +0900

    css: VideoCard styling

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,17 @@
 import React from "react"
+import { formatAgo } from "../util/date";
 
 export default function VideoCard({video}) {
     const {thumbnails, title, channelTitle, publishedAt} = video;
 
     return (
-        <div>
-            <img src={thumbnails.default.url} alt="thumbnail"></img>
-            <h5>{title}</h5>
-            <h6>{channelTitle}</h6>
-            <h6>{publishedAt}</h6>
-        </div>
+        <li>
+            <img className="w-full" src={thumbnails.medium.url} alt={title}></img>
+            <div>
+                <p className="font-semibold mt-2 mb-1 line-clamp-2">{title}</p>
+                <p className="text-sm text-slate-400">{channelTitle}</p>
+                <p className="text-sm text-slate-400">{formatAgo(publishedAt, 'ko')}</p>
+            </div>
+        </li>
     )
 }
--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { createContext } from "react";
+import FakeYoutubeClient from "../api/fake-youtube-client";
 import Youtube from "../api/youtube";
 import YoutubeClient from "../api/youtube-client";
 
 export const YoutubeApiContext = createContext();
-const client = new YoutubeClient();
+const client = new FakeYoutubeClient();
 const youtube = new Youtube(client);
 export function YoutubeApiProvider({ children }) {
   return (
